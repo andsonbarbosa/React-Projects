a3a8df87ac5b0754d1cd3f8e197c7d19d4fb9881
Andson <andsonsilva2015@hotmail.com>
2022-06-20 17:23:19 -0300

    Completed expense tracker.

---
+++ expense-tracker/src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    {
+        date: new Date(2022, 5, 20),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12
+    },
+    {
+        date: new Date(2022, 5, 20),
+        category: 'food',
+        title: 'Buger King',
+        value: 42.12
+    },
+    {
+        date: new Date(2022, 5, 21),
+        category: 'rent',
+        title: 'Aluguel Apt',
+        value: 2234.12
+    },
+    {
+        date: new Date(2022, 7, 20),
+        category: 'salary',
+        title: 'Salário',
+        value: 3200.12
+    }
+];
